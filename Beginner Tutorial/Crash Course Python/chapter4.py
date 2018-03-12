c96e2615a33d9f6c335aa30398a61d456045bdd2
Lauro Cabral <Lauro199471@gmail.com>
2018-03-11 22:50:35 -0700

    Added lab

---
+++ Beginner Tutorial/Crash Course Python/chapter4.py
@@ -0,0 +1,4 @@
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians: # This line tells Python to pull a name from the list magicians, and store it in the variable magician
+    print(magician + ", that was a great trick")        
+    
